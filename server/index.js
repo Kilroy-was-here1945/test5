4af2f1009093b98a960129ba83a8f961fca77b47
Kilroy-was-here1945 <84049904+Kilroy-was-here1945@users.noreply.github.com>
2022-07-05 18:26:44 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,16 +7,16 @@ const {seed, getCountries, getCities, createCity, deleteCity} = require('./contr
 
 app.use(express.json())
 app.use(cors())
+// city data
+app.post('/cities', createCity)
+app.get('/cities', getCities)
+app.delete('/cities/:id', deleteCity)
+// this gabs all countries
+app.get('/countries', getCountries)
 
-// DEV
+// this seeds the repo
 app.post('/seed', seed)
 
-// COUNTRIES
-// app.get('/countries', getCountries)
 
-// CITIES
-// app.post('/cities', createCity)
-// app.get('/cities', getCities)
-// app.delete('/cities/:id', deleteCity)
 
 app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
